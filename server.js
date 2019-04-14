64b3925ad69a009f5e212a0a315a1c5b1fadbce7
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-04-14 11:31:30 -0600

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pokemon = require('./pokemon');
 app.use(bodyParser({extended: true}));
+
+app.get('/pokemon', (req,res)=>{
+    res.send(pokemon);
+  });
 
 
 
